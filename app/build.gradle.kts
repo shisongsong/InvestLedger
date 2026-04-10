67734e5858e6d9f533c6d88a3260ac2bad267d52
嵩嵩和惠惠 <ourhome@songsonghehuihuideMac-mini.local>
2026-04-10 15:13:55 +0800

    fix: upgrade compose-bom to 2024.02.00 for SegmentedButton

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
     
     // Jetpack Compose
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
